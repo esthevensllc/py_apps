1c685b6e4f78d606775013587c5c379b0c4dd96b
Cristhian De La Cruz Linarez <cclinarez@indracompany.com>
2023-05-25 14:49:49 -0500

    agrega shells para nce oracle handlers, inventarios san e inventarios sam5620

---
+++ shell/nce/cargas/load_oracle_handlers_hxh2.sh
@@ -0,0 +1,1 @@
+$PY_APPS_SH_DIR/shared/main_unique.sh src.control_carga.ora_handlers.ResumenEventConsumer ncehxh2
--- shell/san/load_inventarios_sam5620.sh
+++ shell/san/load_inventarios_sam5620.sh
@@ -1,8 +1,10 @@
-/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.physical_link_manager.LoadPhysicalLM_sam5620
-/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.service_manager.LoadServiceManager_sam5620
-/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.network_element.LoadNetworkElement_sam5620
-/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.network_element.LoadL3AccessInterface_sam5620
-/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.lag_interface.LoadLagInterfaces_sam5620
-/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.network_interface.LoadNetworkInterfaces_sam5620
-/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.not_disc_physical_lm.LoadNetworkInterfaces_sam5620
-/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.vprn.LoadVPRN_sam5620
+$PY_APPS_SH_DIR/shared/main_unique.sh src.san.shared.SANInventarioEventConsumer sam_5620
+
+#/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.physical_link_manager.LoadPhysicalLM_sam5620
+#/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.service_manager.LoadServiceManager_sam5620
+#/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.network_element.LoadNetworkElement_sam5620
+#/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.network_element.LoadL3AccessInterface_sam5620
+#/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.lag_interface.LoadLagInterfaces_sam5620
+#/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.network_interface.LoadNetworkInterfaces_sam5620
+#/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.not_disc_physical_lm.LoadNetworkInterfaces_sam5620
+#/index1/tareas/proyectos_python/apps/py_apps/shell/shared/main.sh src.san.vprn.LoadVPRN_sam5620
