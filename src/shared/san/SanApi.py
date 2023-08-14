e6292354d67d214e340f6948468670434a42a169
Cristhian De La Cruz Linarez <cclinarez@indracompany.com>
2023-08-14 13:18:15 -0500

    cambia ip de sam api nokia

--- src/shared/san/SanApi.py
+++ src/shared/san/SanApi.py
@@ -15,7 +15,7 @@ class SanApi:
         #self.token_expiration_date = None
         self.def_headers = {'Content-Type': 'application/xml'}
         self.connections = {
-            'default': {'base_url': "https://172.19.147.71:8443/xmlapi"},
+            'default': {'base_url': "https://172.19.147.69:8443/xmlapi"},
             'sam_5620': {'base_url': "http://10.140.255.1:8080/xmlapi"},
         }
         self.use('default')
