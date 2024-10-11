3f2c4ba76c3a2caada5cb7d0121f64f89d22c449
Cristhian De La Cruz Linarez <cclinarez@indracompany.com>
2024-10-11 11:27:37 -0500

    fix(gde): quita procedures de exec_after_st

--- src/gde/alarms/repository.py
+++ src/gde/alarms/repository.py
@@ -28,8 +28,6 @@ class InMemoryGdeConfigRepository(InMemoryConfigRepository):
                 'exec_after_by': "all",
                 'exec_after_st': """begin
                     pk_alarms_autin_mn.sp_alarm_autin_load;
-                    pk_alarm_autin.sp_alarm_autin_1014;
-                    pk_alarm_autin.sp_alarm_autin_100;
                 end;""",
                 'search_time_ago': '{"days": 1}',
                 'loop_time': '{"minutes": 10}',
