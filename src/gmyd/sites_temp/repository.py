296c7efe0c7f9b749989beaa8afef29d53ef1265
Cristhian De La Cruz Linarez <cclinarez@indracompany.com>
2025-03-10 14:28:57 -0500

    chore(gmyd): actualizar query PSEG_0DATAGMDTX_API

--- src/gmyd/sites_temp/repository.py
+++ src/gmyd/sites_temp/repository.py
@@ -9,13 +9,50 @@ class SitesRepository:
         self.db.query(f'DELETE FROM {self.table}')
 
     def insert_from_array(self, registros):
-        template = f"INSERT INTO {self.table}(CODIGO, NOMBRE, ROUTER_ACCESO, TX, INTEGRACION, ADDDATE) VALUES (:CODIGO, :NOMBRE, :ROUTER_ACCESO, :TX, :INTEGRACION, TRUNC(SYSDATE, 'DD'))"
+        template = f"""INSERT INTO {self.table}(
+        id_site, codigo, nombre, direccion, latitud, longitud, router_agregacion, router_acceso, fecha_creacion,
+        resp_creacion, fecha_cambio, resp_cambio, fecha_baja, resp_baja, altura_torre, altura_predio, altitud,
+        id_tx, id_integ, id_estado, id_torre, id_tipo, ccpp, id_mtc, prob_gub, id_tipo_gub, con_gps, tx,
+        integracion, estado, tipo, permiso_gub,
+        adddate) VALUES (
+        :id_site, :codigo, :nombre, :direccion, :latitud, :longitud, :router_agregacion, :router_acceso, to_date(:fecha_creacion, 'yyyy-mm-dd hh24:mi:ss'),
+        :resp_creacion, to_date(:fecha_cambio, 'yyyy-mm-dd hh24:mi:ss'), :resp_cambio, to_date(:fecha_baja, 'yyyy-mm-dd hh24:mi:ss'), :resp_baja, :altura_torre, :altura_predio, :altitud,
+        :id_tx, :id_integ, :id_estado, :id_torre, :id_tipo, :ccpp, :id_mtc, :prob_gub, :id_tipo_gub, :con_gps, :tx,
+        :integracion, :estado, :tipo, :permiso_gub,
+        TRUNC(SYSDATE, 'DD'))"""
         bindings = {
-            'CODIGO': cx_Oracle.STRING,
-            'NOMBRE': cx_Oracle.STRING,
-            'ROUTER_ACCESO': cx_Oracle.STRING,
-            'TX': cx_Oracle.STRING,
-            'INTEGRACION': cx_Oracle.STRING
+            'id_site': cx_Oracle.NUMBER,
+            'codigo': cx_Oracle.STRING,
+            'nombre': cx_Oracle.STRING,
+            'direccion': cx_Oracle.STRING,
+            'latitud': cx_Oracle.STRING,
+            'longitud': cx_Oracle.STRING,
+            'router_agregacion': cx_Oracle.STRING,
+            'router_acceso': cx_Oracle.STRING,
+            'fecha_creacion': cx_Oracle.STRING,
+            'resp_creacion': cx_Oracle.STRING,
+            'fecha_cambio': cx_Oracle.STRING,
+            'resp_cambio': cx_Oracle.STRING,
+            'fecha_baja': cx_Oracle.STRING,
+            'resp_baja': cx_Oracle.STRING,
+            'altura_torre': cx_Oracle.STRING,
+            'altura_predio': cx_Oracle.STRING,
+            'altitud': cx_Oracle.STRING,
+            'id_tx': cx_Oracle.NUMBER,
+            'id_integ': cx_Oracle.NUMBER,
+            'id_estado': cx_Oracle.NUMBER,
+            'id_torre': cx_Oracle.NUMBER,
+            'id_tipo': cx_Oracle.NUMBER,
+            'ccpp': cx_Oracle.STRING,
+            'id_mtc': cx_Oracle.NUMBER,
+            'prob_gub': cx_Oracle.STRING,
+            'id_tipo_gub': cx_Oracle.NUMBER,
+            'con_gps': cx_Oracle.STRING,
+            'tx': cx_Oracle.STRING,
+            'integracion': cx_Oracle.STRING,
+            'estado': cx_Oracle.STRING,
+            'tipo': cx_Oracle.STRING,
+            'permiso_gub': cx_Oracle.STRING,
         }
         config = {'template': template, 'bindings': bindings, 'row_type': 'object', 'limit_to_commit': 50000}
         self.db.save_from_array2(config, registros)
--- src/gmyd/sites_temp/services.py
+++ src/gmyd/sites_temp/services.py
@@ -20,16 +20,87 @@ class LoadSites:
     def get_data_from_oracle(self):
         # self.oracledb.useConnection("DBOPTDA")
         result = self.dboptda.fetch("""select
-        a.codigo, a.nombre, a.router_acceso, b.tipo tx, c.tipo tipo_integracion from DBOPTDA.tbl_sites a
+        id_site, 
+        codigo, 
+        nombre, 
+        direccion, 
+        latitud, 
+        longitud, 
+        router_agregacion, 
+        router_acceso, 
+        to_char(fecha_creacion, 'yyyy-mm-dd hh24:mi:ss') fecha_creacion, 
+        resp_creacion, 
+        to_char(fecha_cambio, 'yyyy-mm-dd hh24:mi:ss') fecha_cambio, 
+        resp_cambio, 
+        to_char(fecha_baja, 'yyyy-mm-dd hh24:mi:ss') fecha_baja, 
+        resp_baja, 
+        altura_torre, 
+        altura_predio, 
+        altitud, 
+        --id_dep, 
+        --id_prov, 
+        --id_dist, 
+        a.id_tx, 
+        a.id_integ, 
+        a.id_estado, 
+        id_torre, 
+        a.id_tipo, 
+        ccpp, 
+        id_mtc, 
+        prob_gub, 
+        a.id_tipo_gub, 
+        con_gps, 
+        --departamento, 
+        --provincia, 
+        --distrito, 
+        b.tipo tx, 
+        c.tipo integracion, 
+        e.tipo estado,
+        --torre
+        --icon
+        t.tipo tipo,
+        g.tipo permiso_gub
+        --ubigeo
+        from DBOPTDA.tbl_sites a
+        left join dboptda.tbl_sites_estado e on e.id_estado = a.id_estado
+        left join dboptda.tbl_sites_tipo t on t.id = a.id_tipo
+        left join dboptda.tbl_sites_tipo_gubernam g on g.id = a.id_tipo_gub
         left join DBOPTDA.tbl_tipo_tx b on a.id_tx=b.id_tx
         left join DBOPTDA.tbl_tipo_integracion c on a.id_integ=c.id_integ""")
         data = []
         for row in result:
             data.append({
-                'CODIGO': row[0],
-                'NOMBRE': row[1],
-                'ROUTER_ACCESO': row[2],
-                'TX': row[3],
-                'INTEGRACION': row[4]
+                'id_site': row[0],
+                'codigo': row[1],
+                'nombre': row[2],
+                'direccion': row[3],
+                'latitud': row[4],
+                'longitud': row[5],
+                'router_agregacion': row[6],
+                'router_acceso': row[7],
+                'fecha_creacion': row[8],
+                'resp_creacion': row[9],
+                'fecha_cambio': row[10],
+                'resp_cambio': row[11],
+                'fecha_baja': row[12],
+                'resp_baja': row[13],
+                'altura_torre': row[14],
+                'altura_predio': row[15],
+                'altitud': row[16],
+                'id_tx': row[17],
+                'id_integ': row[18],
+                'id_estado': row[19],
+                'id_torre': row[20],
+                'id_tipo': row[21],
+                'ccpp': row[22],
+                'id_mtc': row[23],
+                'prob_gub': row[24],
+                'id_tipo_gub': row[25],
+                'con_gps': row[26],
+                'tx': row[27],
+                'integracion': row[28],
+                'estado': row[29],
+                'tipo': row[30],
+                'permiso_gub': row[31],
             })
         return data
