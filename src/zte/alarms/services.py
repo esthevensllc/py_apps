b118b843a411f0ff86b5a947a872dd0e8d0152ef
Cristhian De La Cruz Linarez <cclinarez@indracompany.com>
2025-06-18 11:43:56 -0500

    feat(zte): actualizar propiedad max_jobs_per_run=25

--- src/zte/alarms/services.py
+++ src/zte/alarms/services.py
@@ -30,7 +30,7 @@ class ZTEEventConsumerFromConfig(SimpleEventConsumer):
         self.repository = repository
         self.loop = False
         self.carga_config = {}
-        self.max_jobs_per_run = 10
+        self.max_jobs_per_run = 25
 
     def execute(self, group_id=None):
         cargas = []
