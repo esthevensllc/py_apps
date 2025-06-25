c3274603e99e22cc5d57439d143df3e2ad029bb6
Cristhian De La Cruz Linarez <cclinarez@indracompany.com>
2025-06-25 14:41:35 -0500

    fix(webacs): corregir WebacsEventConsumerFromConfig

--- src/webacs/api/services.py
+++ src/webacs/api/services.py
@@ -373,4 +373,4 @@ class WebacsEventConsumerFromConfig(EventConsumerFromConfig):
     def __init__(self, queue_service, app_container, notification_service, repository):
         super().__init__(queue_service, app_container, notification_service, repository)
         self.handler_name = LOAD_WEBACS_FROM_CONFIG
-        self.max_check_attemps = 0
+        self.max_check_attemps = 1
