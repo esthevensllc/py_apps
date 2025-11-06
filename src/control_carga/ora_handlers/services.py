783ba36a63fbbb7f5e6549879669b0ab6d958619
Cristhian De La Cruz Linarez <cclinarez@indracompany.com>
2025-11-06 13:07:18 -0500

    feat(control_carga): agregar flujo para cargas cada 15min

--- src/control_carga/ora_handlers/services.py
+++ src/control_carga/ora_handlers/services.py
@@ -30,11 +30,14 @@ class LoadOracleHandlers:
         for row in cargas:
             handlers = self.repository.get_ora_handlers_by_proyecto(row['proyecto'], config['format'])
             p_farchivo_hxh = row['fecha']
+            p_farchivo_fin_15min = row['fecha'] + dt.timedelta(minutes=15)
             p_farchivo_fin_hxh = row['fecha'] + dt.timedelta(hours=1)
             p_farchivo_fin_dxd = row['fecha'] + dt.timedelta(days=1)
             def_params = {
                 'p_farchivo_hxh': p_farchivo_hxh.strftime('%Y-%m-%d %H:%M:%S'),
                 'p_farchivo_fin_hxh': p_farchivo_fin_hxh.strftime('%Y-%m-%d %H:%M:%S'),
+                'p_farchivo_min_f1': p_farchivo_hxh.strftime('%d/%m/%Y %H:%M'),
+                'p_farchivo_fin_15min_f1': p_farchivo_fin_hxh.strftime('%d/%m/%Y %H:%M'),
                 'p_farchivo_hxh_f1': p_farchivo_hxh.strftime('%d/%m/%Y %H'),
                 'p_farchivo_fin_hxh_f1': p_farchivo_fin_hxh.strftime('%d/%m/%Y %H'),
                 'p_farchivo_dxd_f1': p_farchivo_hxh.strftime('%d/%m/%Y'),
